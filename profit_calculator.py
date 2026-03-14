d093b78797be5a2c10a06afd276b8408caa2717f
dipak1920basnet <dipak1920basnet@gmail.com>
2026-03-14 14:05:25 +0545

    calculate profit

---
+++ profit_calculator.py
@@ -0,0 +1,21 @@
+# Stores information 
+revenue = 1250
+cost = 930
+
+"""
+Program must:
+
+1️⃣ compute profit
+2️⃣ print result
+3️⃣ print message:
+"""
+
+# compute profit
+profit = revenue - cost
+print(f"prifit: {profit}")
+
+if profit > 0:
+    print("Profitable business")
+else:
+    print("Loss detected")
+    

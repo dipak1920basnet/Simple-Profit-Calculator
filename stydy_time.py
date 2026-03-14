c1af27b55df42bbf8d9c4c3f1129b316976dff7a
dipak1920basnet <dipak1920basnet@gmail.com>
2026-03-14 14:11:15 +0545

    calculate the average study horus

---
+++ stydy_time.py
@@ -0,0 +1,14 @@
+# Stores data 
+day1 = 3
+day2 = 4
+day3 = 2
+day4 = 5
+day5 = 3
+
+# process and stores information
+total_study_hours = (day1+day2+day3+day4+day5)
+
+average_study_hours = total_study_hours/5
+
+if average_study_hours<3:
+    print("You are not studying enough")
